ee096e179c6d11035f6c60e2c846f1e648c5997f
ai-kana <lilyclark823@gmail.com>
2024-12-05 00:54:08 -0800

    update unturnov func

--- .tmux.sh
+++ .tmux.sh
@@ -2,8 +2,8 @@
 # Functions to open commonly used tmux sessions
 #
 
-function tm_unturnov() {
-    session = "unturnov"
+function tmkronstadt() {
+    session = "kronstadt"
 
     if tmux has-session -t $session 2> /dev/null; then 
         tmux attach-session -t $session
@@ -11,25 +11,8 @@ function tm_unturnov() {
         tmux new-session -d -s $session "ls"
         tmux new-window -t $session:2
 
-        tmux send-keys -t $session:1 'cd ~/code/cs/Unturnov;nvim .' C-m
+        tmux send-keys -t $session:1 'cd ~/code/cs/Kronstadt;nvim .' C-m
         tmux send-keys -t $session:2 'cd ~/U3DS/Servers/Unturnov' C-m
-
-        tmux select-window  -t $session:1
-        tmux a
-    fi
-}
-
-function tm_com() {
-    session = "com"
-
-    if tmux has-session -t $session 2> /dev/null; then 
-        tmux attach-session -t $session
-    else
-        tmux new-session -d -s $session "ls"
-        tmux new-window -t $session:2
-
-        tmux send-keys -t $session:1 'cd ~/code/cs/Comissions' C-m
-        tmux send-keys -t $session:2 'cd ~/U3DS/Servers' C-m
 
         tmux select-window  -t $session:1
         tmux a
