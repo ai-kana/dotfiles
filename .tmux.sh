a4967a23bc001598d8d2fcd571367ab2d39703a5
ai-kana <lilyclark823@gmail.com>
2025-01-08 15:05:57 -0800

    Add localmod tmux session

--- .tmux.sh
+++ .tmux.sh
@@ -11,8 +11,25 @@ function tmmeow() {
         tmux new-session -d -s $session "ls"
         tmux new-window -t $session:2
 
-        tmux send-keys -t $session:1 'cd ~/code/cs/Meow;nvim .' C-m
+        tmux send-keys -t $session:1 'cd ~/code/cs/Meow' C-m
         tmux send-keys -t $session:2 'cd ~/U3DS/Servers/Unturnov' C-m
+
+        tmux select-window  -t $session:1
+        tmux a
+    fi
+}
+
+function tmlocalmod() {
+    session = "localmod"
+
+    if tmux has-session -t $session 2> /dev/null; then 
+        tmux attach-session -t $session
+    else
+        tmux new-session -d -s $session "ls"
+        tmux new-window -t $session:2
+
+        tmux send-keys -t $session:1 'cd ~/code/cs/LocalMod' C-m
+        tmux send-keys -t $session:2 'cd ~/U3DS/Servers/LocalModTest' C-m
 
         tmux select-window  -t $session:1
         tmux a
